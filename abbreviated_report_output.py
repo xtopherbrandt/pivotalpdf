4c0bcf5f2460f23529d70c8a1c36cec65e0c5826
xtopher <xtopher@1ff268e8-c99e-e111-8f63-00e0815b4126>
2012-06-04 21:43:22 +0000

    Extend cache fix to abbreviated report output

--- abbreviated_report_output.py
+++ abbreviated_report_output.py
@@ -171,7 +171,7 @@ class AbbreviatedReportOutput():
    
       # Set the http headers
       httpResponse.headers['Content-Type'] = 'application/pdf'
-      httpResponse.headers['Pragma'] = 'public'
+      httpResponse.headers['Pragma'] = 'private'
       httpResponse.headers['Cache-Control'] = 'maxage=1'
       httpResponse.headers['Content-Disposition'] = """inline; filename='{0}'""".format(filename)
       httpResponse.headers['Accept-Ranges'] = 'bytes'
